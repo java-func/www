8877394cd419853b128a0e7db00cde351c968c51
tom-sapletta-com <tom@sapletta.com>
2022-05-08 17:11:52 +0200

    update menu url

--- readme.sh
+++ readme.sh
@@ -13,14 +13,18 @@ LOGS="readme.logs.txt"
 PROJECTS=readme.txt
 PROJECT_LIST=$(cat $PROJECTS)
 LOCAL_PATH=$(pwd)
+SUBFOLDER="DOCS"
+MENU_URL="$SUBFOLDER/PROJECTS.md"
+MENU_PATH="$SUBFOLDER/PROJECTS_LOCAL.md"
+OUTPUT="README.md"
 
 # START
 echo "`date +"%T.%3N"` START" > $LOGS
 echo "$(date +"%T.%3N") CREATE_MENU" >> $LOGS
 #
 DOMAIN=$(cat CNAME)
-echo "+ [$DOMAIN](http://$DOMAIN)" > DOCS/PROJECTS.md
-echo "+ [$LOCAL_PATH](file://$LOCAL_PATH/)" > DOCS/PROJECTS_LOCAL.md
+echo "+ [$DOMAIN](http://$DOMAIN)" > $MENU_URL
+echo "+ [$LOCAL_PATH](file://$LOCAL_PATH/)" > $MENU_PATH
 #
 for FILE in */in.md; do
   line=$(head -n 1 $FILE)
@@ -28,18 +32,18 @@ for FILE in */in.md; do
   #echo PROTOCOL=${URL%%://*}
   NAME=${FILE%%/*}
   URL=$DOMAIN/$NAME
-  echo "+ [$NAME $line](http://$URL)" >> DOCS/PROJECTS.md
-  echo "+ [$NAME $line](file://$LOCAL_PATH/$NAME/index.html)" >> DOCS/PROJECTS_LOCAL.md
+  echo "+ [$NAME $line](http://$URL)" >> $MENU_URL
+  echo "+ [$NAME $line](file://$LOCAL_PATH/$NAME/index.html)" >> $MENU_PATH
 done
 
 ## combine from another sites
 echo "$(date +"%T.%3N") COMBINE_FILES" >> $LOGS
 
-echo "" > README.md
+echo "" > $OUTPUT
 for project in $PROJECT_LIST; do
   echo "$(date +"%T.%3N") COMBINE_FILE $project" >> $LOGS
-  cat $project >> README.md
-  echo "" >> README.md
+  cat $project >> $OUTPUT
+  echo "" >> $OUTPUT
 done
 # Send to github
 #git commit -m "Combine and update docs"
